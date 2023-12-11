99ab45dedb5c045b19ab1be08c346b6039e562c8
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-12-11 17:19:58 +0200

    update bin

--- src/Azure02/obj/Debug/net8.0/Azure02.AssemblyInfo.cs
+++ src/Azure02/obj/Debug/net8.0/Azure02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Azure02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4da3cb644c72d20d22010e414e7e5b636024c81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Azure02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Azure02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
